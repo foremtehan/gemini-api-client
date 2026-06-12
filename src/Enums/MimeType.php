df45c61cce73aa34f438f0f85c3d7bc2067ee390
foremtehan <53290883+foremtehan@users.noreply.github.com>
2026-06-12 21:45:45 +0330

    Fix spacing in AUDIO_L16_RATE_24000_CHANNELS_1
    Updated the MIME type for AUDIO_L16_RATE_24000_CHANNELS_1 to include spaces.

--- src/Enums/MimeType.php
+++ src/Enums/MimeType.php
@@ -24,7 +24,7 @@ enum MimeType: string
     case AUDIO_OGG = 'audio/ogg';
     case AUDIO_FLAC = 'audio/flac';
     case AUDIO_L16_PCM_RATE_24000 = 'audio/L16;codec=pcm;rate=24000';
-    case AUDIO_L16_RATE_24000_CHANNELS_1 = 'audio/l16;rate=24000;channels=1';
+    case AUDIO_L16_RATE_24000_CHANNELS_1 = 'audio/l16; rate=24000; channels=1';
     // Video
     case VIDEO_MP4 = 'video/mp4';
     case VIDEO_MPEG = 'video/mpeg';
